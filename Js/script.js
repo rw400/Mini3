2812c4852111ccb3a572a7dee4f84d7b8f91e0e0
rw400 <ronniewong5@gmail.com>
2017-03-04 23:38:51 -0500

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+/*document.getElementById('demo').innerHTML = 'Hello JavaScript';
+Writing into an HTML element, using innerHTML.
+Writing into the HTML output using document.write().
+Writing into an alert box, using window.alert().
+Writing into the browser console, using console.log().
+*/
+
+/*create method which is called in the testing html
+changes behaviour of the text*/
+function changeFunction() {
+    document.getElementById("demo1").innerHTML = "Paragraph changed.";
+}
+
+
+document.write("I'm going to work on this more in the near future on the own time. Sorry, Stay Tuned.");
+/*displays message box*/
+alert("Sorry I did not get too far with Javascript!");
